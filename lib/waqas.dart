e7e2083d45ad434c7f268d611507159f29d05e91
waqasyahya <waqasyahya143s@gmail.com>
2024-04-16 13:06:14 +0500

    first commit

--- lib/waqas.dart
+++ lib/waqas.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 class waqas extends StatelessWidget {
   const waqas({super.key});
 
@@ -6,11 +7,51 @@ class waqas extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar:  AppBar(
+        backgroundColor: Colors.blue,
         centerTitle: true,
-        title: Text('hi my name is waqas'),
+        title:SvgPicture.asset(
+            'assets/icon/house.svg',
+            semanticsLabel: 'Acme Logo',
+
+            color: Colors.white,
+        ),
         leading: Text('waqas'),
         actions: [IconButton(onPressed: (){}, icon: Icon(Icons.more_vert, color: Colors.red,))],
       ),
+      body: Column(
+        children: [
+
+          iconwidget(60, 'ajkdh','usman'),
+          iconwidget(60,'ajkdh','usmaan'),
+          iconwidget(60,'ajkdh','ali'),
+        ],
+      ),
+    );
+  }
+}
+class iconwidget extends StatelessWidget {
+  double height;
+String name;
+  String name1;
+
+
+  iconwidget(this.height, this.name, this.name1);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SvgPicture.asset(
+          'assets/icon/house.svg',
+          semanticsLabel: 'Acme Logo',
+          height: height,
+          color: Colors.red,
+
+        ),
+
+        Text(name1),
+        Text(name)
+      ],
     );
   }
 }
