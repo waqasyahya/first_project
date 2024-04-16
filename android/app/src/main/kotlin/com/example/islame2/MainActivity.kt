57aad2b77bce238cbf47fad6fe089616a14d45c1
waqasyahya <waqasyahya143s@gmail.com>
2024-04-16 12:24:20 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/islame2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.islame2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/waqas.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class waqas extends StatelessWidget {
+  const waqas({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:  AppBar(
+        centerTitle: true,
+        title: Text('hi my name is hamza'),
+        leading: Text('waqas'),
+        actions: [IconButton(onPressed: (){}, icon: Icon(Icons.more_vert, color: Colors.red,))],
+      ),
+    );
+  }
+}
